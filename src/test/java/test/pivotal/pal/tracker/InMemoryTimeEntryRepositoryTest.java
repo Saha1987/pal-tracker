736c9a5fe674d64b2bbc00c8015ef5d2a02c6f7d
pal_user <ace_14r@yahoo.co.in>
2018-05-22 09:12:28 -0600

    upated

--- src/test/java/test/pivotal/pal/tracker/InMemoryTimeEntryRepositoryTest.java
+++ src/test/java/test/pivotal/pal/tracker/InMemoryTimeEntryRepositoryTest.java
@@ -1,7 +1,8 @@
 package test.pivotal.pal.tracker;
 
-import io.pivotal.pal.tracker.InMemoryTimeEntryRepository;
-import io.pivotal.pal.tracker.TimeEntry;
+import io.pivotal.tracker.InMemoryTimeEntryRepository;
+import io.pivotal.tracker.TimeEntry;
+import io.pivotal.tracker.TimeEntry;
 import org.junit.Test;
 
 import java.time.LocalDate;
